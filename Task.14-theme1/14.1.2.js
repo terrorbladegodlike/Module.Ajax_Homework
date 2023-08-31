15de1ca931f4c3cc995178f269d8185037e30fd0
terrorbladegodlike <ternovetchii.2002@mail.ru>
2023-08-31 18:38:10 +0300

    All

---
+++ Task.14-theme1/14.1.2.js
@@ -0,0 +1,36 @@
+const jsonString = `
+{
+    "list": [
+        {
+            "name": "Petr",
+            "age": "20",
+            "prof": "mechanic"
+        },
+        {
+            "name": "Vova",
+            "age": "60",
+            "prof": "pilot"
+        }
+    ]
+}
+`
+
+const result = {
+  list: []
+}
+
+const data = JSON.parse(jsonString);
+const list = data.list;
+
+
+for (let item of list) {
+  result.list.push(
+    {
+      name: item.name,
+      age: Number(item.age),
+      prof: item.prof
+    },
+  )
+}
+
+console.log(result)
---
+++ Task.14-theme2/14.2.1.js
@@ -0,0 +1,49 @@
+function useRequest(url, callback) {
+  let xhr = new XMLHttpRequest();
+  xhr.open('get', url, true);
+
+  xhr.onload = function () {
+    if (xhr.status != 200) {
+      console.log('Статус ответа: ', xhr.status);
+    } else {
+      const result = JSON.parse(xhr.response);
+      if (callback) {
+        callback(result)
+      }
+    }
+  };
+
+  xhr.onerror = function () {
+    console.log('Ошибка! Статус ответа: ', xhr.status);
+  };
+
+  xhr.send();
+};
+
+const btnNode = document.querySelector('button');
+const resultNode = document.querySelector('.result');
+
+function showResult(apiData) {
+  let cards = '';
+  apiData.forEach(item => {
+    const cardBlock = `
+          <div class="card">
+              <img class="card-image" src="${item.download_url}">
+              <p>${item.author}</p>
+          </div>
+      `;
+    cards += cardBlock;
+  });
+
+  resultNode.innerHTML = cards;
+}
+
+btnNode.addEventListener('click', () => {
+  const value = document.querySelector('input').value;
+  if (value < 1 || value > 10) {
+    resultNode.innerHTML = "<p>Число находится вне диапазона от 1 до 10!</p>";
+  } else {
+    url = `https://picsum.photos/v2/list?limit=${value}`;
+    useRequest(url, showResult)
+  }
+})
